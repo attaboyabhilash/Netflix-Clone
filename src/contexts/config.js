ac4a5eba70ff64e7c5d51f9adddf7ae1895c73a2
Abhilash Negi <tiger.abhi.3609@gmail.com>
2020-10-30 11:52:04 +0530

    final commit

---
+++ src/contexts/config.js
@@ -0,0 +1,10 @@
+export default config = {
+    apiKey: "AIzaSyDDCOCFkAS9Im3l7cjy4sh6MMPlMiQPn9k",
+    authDomain: "netflix-clone-3609.firebaseapp.com",
+    databaseURL: "https://netflix-clone-3609.firebaseio.com",
+    projectId: "netflix-clone-3609",
+    storageBucket: "netflix-clone-3609.appspot.com",
+    messagingSenderId: "862778083382",
+    appId: "1:862778083382:web:f071aba10e903ab9d7b097",
+    measurementId: "G-237K4C5X5X"
+};
